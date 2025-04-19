4412894016fe76436082da3d3999c6f5f98f5d49
qwertryryryry <romantond9mak@gmail.com>
2025-04-19 18:27:08 +0500

    Add files via upload

---
+++ ping_pong.py
@@ -0,0 +1,89 @@
+from pygame import *
+
+back = (255, 87, 168)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+class GameSpite(sprite.Sprite):
+    def _init_(self, player_img, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_img), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect = player_x
+        self.rect = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSpite):
+    def move_right(self):
+        keys=key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height-50:
+            self.rect.y += self.speed
+
+    def move_left(self):
+        keys=key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height-50:
+            self.rect.y += self.speed
+
+    
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('1 проиграл', True, (69, 2, 35))
+lose1 = font.render('2 проиграл', True, (69, 2, 35))
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSpite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        racket1.move_right()
+        racket2.move_left()
+        ball.rect.x += speed_x             
+        ball.rect.y += speed_y             
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-20 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if ball.rect.x <0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True 
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True 
+        
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
+
